062c480ab680693f161243b36e219adc7fa258a9
danielapostolachi <danapostolaki23@gmail.com>
2025-05-20 01:46:05 +0300

    Added images

---
+++ businessLogic/Migrations/202505192141457_TryingUser.cs
@@ -0,0 +1,34 @@
+﻿namespace businessLogic.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class TryingUser : DbMigration
+    {
+        public override void Up()
+        {
+            RenameTable(name: "dbo.TBLUserInfo", newName: "UserInfoes");
+            DropPrimaryKey("dbo.UserInfoes");
+
+            // Add the new Id column with identity = true
+            AddColumn("dbo.UserInfoes", "Id", c => c.Int(nullable: false, identity: true));
+
+            // Add the other columns as usual
+            AddColumn("dbo.UserInfoes", "FirstName", c => c.String());
+            AddColumn("dbo.UserInfoes", "LastName", c => c.String());
+            AddColumn("dbo.UserInfoes", "Username", c => c.String());
+            AddColumn("dbo.UserInfoes", "Email", c => c.String());
+            AddColumn("dbo.UserInfoes", "PasswordHash", c => c.String());
+
+            // Set the primary key on the new identity column
+            AddPrimaryKey("dbo.UserInfoes", "Id");
+
+            // Now drop the old columns (including the old identity column)
+            DropColumn("dbo.UserInfoes", "IdUs");
+            DropColumn("dbo.UserInfoes", "UsernameUS");
+            DropColumn("dbo.UserInfoes", "PasswordUs");
+            DropColumn("dbo.UserInfoes", "Role");
+
+        }
+    }
+}
---
+++ lab_1_templates_sandu/Controllers/RegisterUserController.cs
@@ -0,0 +1,62 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using System.Web.Mvc;
+using lab_1_templates_sandu.Models;
+using eUseControlBussinessLogic.Core;
+using eUseControl.Domain.Entities.User;
+using System;
+
+namespace lab_1_templates_sandu.Controllers
+{
+    public class RegisterUserController : Controller
+    {
+        private readonly UserApi _userApi = new UserApi();
+
+        public ActionResult RegisterPage()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult RegisterPage(UserModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new UserInfo
+                {
+                    FirstName = model.FirstName,
+                    LastName = model.LastName,
+                    Username = model.Username,
+                    Email = model.Email,
+                    PasswordHash = HashPassword(model.Password)
+                };
+
+                bool result = _userApi.RegisterUser(user);
+                if (result)
+                {
+                    return RedirectToAction("RegisterSuccess");
+                }
+
+                ModelState.AddModelError("", "User with this email or username already exists.");
+            }
+
+            return View(model);
+        }
+
+        private string HashPassword(string password)
+        {
+            using (var sha256 = SHA256.Create())
+            {
+                var bytes = Encoding.UTF8.GetBytes(password);
+                var hash = sha256.ComputeHash(bytes);
+                return Convert.ToBase64String(hash);
+            }
+        }
+
+        public ActionResult RegisterSuccess()
+        {
+            return View();
+        }
+    }
+}
