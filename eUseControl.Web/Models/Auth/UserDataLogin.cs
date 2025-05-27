641a189c6303a9f0dc10264e71612484db7d6463
Uzun Paula <uzun.paula777@gmail.com>
2025-05-27 22:57:03 +0300

    refactoring

---
+++ eUseControl.Web/Models/Auth/UserDataLogin.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace eUseControl.Web.Models.Auth
+{
+	public class UserDataLogin
+	{
+        [Required]
+        [Display(Name = "Username or Email")]
+        public string NameOrEmail { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
