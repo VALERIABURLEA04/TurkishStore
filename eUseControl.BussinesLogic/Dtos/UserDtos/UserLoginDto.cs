0ef746035273e563c1dde43fd7be80014d43d5a3
UzunPaula <uzun.paula777@gmail.com>
2025-06-13 23:32:03 +0300

    Removed Username from Login display properties

--- eUseControl.BussinesLogic/Dtos/UserDtos/UserLoginDto.cs
+++ eUseControl.BussinesLogic/Dtos/UserDtos/UserLoginDto.cs
@@ -6,13 +6,14 @@ namespace businessLogic.Dtos.UserDtos
     public class UserLoginDto
     {
         [Required]
-        [Display(Name = "Username or Email")]
+        [Display(Name = "Email")]
         public string NameOrEmail { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
+
         public DateTime LoginDataTime { get; set; }
     }
 }
